3052886fcc82487fdbf2fe168e99872a51a256cd
sago35 <sago35@gmail.com>
2020-08-14 16:02:59 +0900

    Initial

---
+++ dma.go
@@ -0,0 +1,357 @@
+// +build atsamd51
+
+package dma
+
+import (
+	"device/sam"
+	"fmt"
+	"runtime/interrupt"
+	"unsafe"
+)
+
+type DMA struct {
+	Channel uint8
+}
+
+type DMADescriptor struct {
+	btctrl   uint16
+	btcnt    uint16
+	srcaddr  uint32 //unsafe.Pointer
+	dstaddr  uint32 //unsafe.Pointer
+	Descaddr uint32 //unsafe.Pointer
+}
+
+const (
+	dmaDescriptors       = 32
+	maxDMAchannels       = 32
+	maxDMATriggerSources = 32
+)
+
+//go:align 16
+var DmaDescriptorSection [dmaDescriptors]DMADescriptor
+
+//go:align 16
+var dmaDescriptorWritebackSection [dmaDescriptors]DMADescriptor
+
+var (
+	nextDMAIndex uint8
+	DMAChannels  [maxDMAchannels]DMA
+	dmaCallbacks [maxDMAchannels]func(DMA)
+)
+
+func handleDMACInterrupt(intr interrupt.Interrupt) {
+	//fmt.Printf("interrupt %#v %04X\r\n", intr, sam.DMAC.INTPEND.Get())
+	//susp := sam.DMAC.INTPEND.HasBits(sam.DMAC_INTPEND_SUSP_Pos)
+	//tcmpl := sam.DMAC.INTPEND.HasBits(sam.DMAC_INTPEND_TCMPL_Pos)
+	//terr := sam.DMAC.INTPEND.HasBits(sam.DMAC_INTPEND_TERR_Pos)
+	//channel := (sam.DMAC.INTPEND.Get() >> sam.DMAC_INTPEND_ID_Pos) & sam.DMAC_INTPEND_ID_Msk
+	channel := (sam.DMAC.INTPEND.Get() >> sam.DMAC_INTPEND_ID_Pos) & sam.DMAC_INTPEND_ID_Msk
+
+	if sam.DMAC.CHANNEL[channel].CHINTFLAG.HasBits(sam.DMAC_CHANNEL_CHINTFLAG_SUSP) {
+		sam.DMAC.CHANNEL[channel].CHINTFLAG.Set(sam.DMAC_CHANNEL_CHINTFLAG_SUSP)
+	}
+	if sam.DMAC.CHANNEL[channel].CHINTFLAG.HasBits(sam.DMAC_CHANNEL_CHINTFLAG_TCMPL) {
+		sam.DMAC.CHANNEL[channel].CHINTFLAG.Set(sam.DMAC_CHANNEL_CHINTFLAG_TCMPL)
+		dmaCallbacks[channel](DMAChannels[channel])
+	}
+	if sam.DMAC.CHANNEL[channel].CHINTFLAG.HasBits(sam.DMAC_CHANNEL_CHINTFLAG_TERR) {
+		sam.DMAC.CHANNEL[channel].CHINTFLAG.Set(sam.DMAC_CHANNEL_CHINTFLAG_TERR)
+	}
+}
+
+func NewDMA(callback func(DMA)) *DMA {
+	if maxDMAchannels <= nextDMAIndex {
+		return nil
+	}
+
+	{
+		// DMAC peripheral has not yet been initialized. Initialize it now.
+		// Init DMAC.
+		// First configure the clocks, then configure the DMA descriptors. Those
+		// descriptors must live in SRAM and must be aligned on a 16-byte boundary.
+		// http://www.lucadavidian.com/2018/03/08/wifi-controlled-neo-pixels-strips/
+		// https://svn.larosterna.com/oss/trunk/arduino/zerotimer/zerodma.cpp
+		sam.MCLK.AHBMASK.SetBits(sam.MCLK_AHBMASK_DMAC_)
+
+		sam.DMAC.CTRL.ClearBits(sam.DMAC_CTRL_DMAENABLE)
+		sam.DMAC.CTRL.SetBits(sam.DMAC_CTRL_SWRST)
+		for sam.DMAC.CTRL.HasBits(sam.DMAC_CTRL_SWRST) {
+		}
+
+		sam.DMAC.BASEADDR.Set(uint32(uintptr(unsafe.Pointer(&DmaDescriptorSection))))
+		sam.DMAC.WRBADDR.Set(uint32(uintptr(unsafe.Pointer(&dmaDescriptorWritebackSection))))
+		fmt.Printf("BASE %08X\r\n", uint32(uintptr(unsafe.Pointer(&DmaDescriptorSection))))
+		fmt.Printf("WRBA %08X\r\n", uint32(uintptr(unsafe.Pointer(&dmaDescriptorWritebackSection))))
+
+		sam.DMAC.CTRL.SetBits(sam.DMAC_CTRL_LVLEN0 | sam.DMAC_CTRL_LVLEN1 | sam.DMAC_CTRL_LVLEN2 | sam.DMAC_CTRL_LVLEN3)
+
+		interrupt.New(sam.IRQ_DMAC_0, handleDMACInterrupt).Enable()
+		interrupt.New(sam.IRQ_DMAC_1, handleDMACInterrupt).Enable()
+		interrupt.New(sam.IRQ_DMAC_2, handleDMACInterrupt).Enable()
+		interrupt.New(sam.IRQ_DMAC_3, handleDMACInterrupt).Enable()
+		interrupt.New(sam.IRQ_DMAC_OTHER, handleDMACInterrupt).Enable()
+
+	}
+
+	dma := DMAChannels[nextDMAIndex]
+	dma.Channel = nextDMAIndex
+	dmaCallbacks[nextDMAIndex] = callback
+
+	// Enable peripheral with all priorities.
+	sam.DMAC.CTRL.SetBits(sam.DMAC_CTRL_DMAENABLE)
+
+	nextDMAIndex++
+	return &dma
+}
+
+func (dma DMA) SetTrigger(triggerSource uint8) error {
+	if maxDMATriggerSources <= triggerSource {
+		return fmt.Errorf("trigger source must be smaller than 32")
+	}
+	sam.DMAC.CHANNEL[dma.Channel].CHCTRLA.ReplaceBits(uint32(triggerSource), sam.DMAC_CHANNEL_CHCTRLA_TRIGSRC_Msk, sam.DMAC_CHANNEL_CHCTRLA_TRIGSRC_Pos)
+
+	return nil
+}
+
+func (dma DMA) AddDescriptor(src unsafe.Pointer, dst unsafe.Pointer, beatSize uint8, srcInc, dstInc bool, stepSize uint8, stepSrc bool, size uint16) {
+	si := uint16(0)
+	if srcInc {
+		si = 1
+	}
+
+	di := uint16(0)
+	if dstInc {
+		di = 1
+	}
+
+	ssel := uint16(0)
+	if stepSrc {
+		ssel = 1
+	}
+
+	ss := uint16(0)
+	switch stepSize {
+	case 1:
+		ss = 0
+	case 2:
+		ss = 1
+	case 4:
+		ss = 2
+	case 8:
+		ss = 3
+	case 16:
+		ss = 4
+	case 32:
+		ss = 5
+	case 64:
+		ss = 6
+	case 128:
+		ss = 7
+	default:
+		// TODO: error
+		ss = 0
+	}
+
+	bs := uint16(0)
+	switch beatSize {
+	case 1:
+		bs = 0
+	case 2:
+		bs = 1
+	case 4:
+		bs = 2
+	default:
+		// TODO: error
+		bs = 0
+	}
+
+	// 1
+	DmaDescriptorSection[dma.Channel] = DMADescriptor{
+		btctrl: (1 << 0) | // VALID: Descriptor Valid
+			(0 << 1) | // EVOSEL=DISABLE: Event Output Selection
+			(1 << 3) | // BLOCKACT=NOACT: Block Action
+			(bs << 8) | // BEATSIZE: Beat Size
+			(si << 10) | // SRCINC: Source Address Increment Enable
+			(di << 11) | // DSTINC: Destination Address Increment Enable
+			(ssel << 12) | // STEPSEL: Step Selection
+			(ss << 13), // STEPSIZE: Address Increment Step Size
+		btcnt:    size >> bs,
+		Descaddr: 0,
+	}
+
+	if stepSrc {
+		// STEPSEL == SRC
+		DmaDescriptorSection[dma.Channel].srcaddr = uint32(uintptr(src) + uintptr((size)<<ss))
+		DmaDescriptorSection[dma.Channel].dstaddr = uint32(uintptr(dst) + uintptr(size))
+	} else {
+		// STEPSEL == DST
+		DmaDescriptorSection[dma.Channel].srcaddr = uint32(uintptr(src) + uintptr(size))
+		DmaDescriptorSection[dma.Channel].dstaddr = uint32(uintptr(dst) + uintptr((size)<<ss))
+	}
+	fmt.Printf("%#v\r\n", DmaDescriptorSection[dma.Channel])
+}
+
+func (dma DMA) Start() {
+	// Reset channel.
+	sam.DMAC.CHANNEL[dma.Channel].CHCTRLA.ClearBits(sam.DMAC_CHANNEL_CHCTRLA_ENABLE)
+	for sam.DMAC.CHANNEL[dma.Channel].CHCTRLA.HasBits(sam.DMAC_CHANNEL_CHCTRLA_ENABLE) {
+	}
+
+	sam.DMAC.CHANNEL[dma.Channel].CHCTRLA.SetBits(sam.DMAC_CHANNEL_CHCTRLA_SWRST)
+	for sam.DMAC.CHANNEL[dma.Channel].CHCTRLA.HasBits(sam.DMAC_CHANNEL_CHCTRLA_SWRST) {
+	}
+
+	// Configure channel.
+	sam.DMAC.CHANNEL[dma.Channel].CHINTENSET.SetBits(sam.DMAC_CHANNEL_CHINTENSET_SUSP | sam.DMAC_CHANNEL_CHINTENSET_TCMPL | sam.DMAC_CHANNEL_CHINTENSET_TERR)
+	//sam.DMAC.CHANNEL[dma.Channel].CHINTENSET.SetBits(sam.DMAC_CHANNEL_CHINTENSET_TCMPL | sam.DMAC_CHANNEL_CHINTENSET_TERR)
+
+	sam.DMAC.CHANNEL[dma.Channel].CHPRILVL.Set(0)
+
+	// for block
+	//sam.DMAC.CHANNEL[dma.Channel].CHCTRLA.Set((sam.DMAC_CHANNEL_CHCTRLA_TRIGACT_BLOCK << sam.DMAC_CHANNEL_CHCTRLA_TRIGACT_Pos) | (sam.DMAC_CHANNEL_CHCTRLA_BURSTLEN_SINGLE << sam.DMAC_CHANNEL_CHCTRLA_BURSTLEN_Pos))
+
+	// for transaction
+	sam.DMAC.CHANNEL[dma.Channel].CHCTRLA.Set(
+		(sam.DMAC_CHANNEL_CHCTRLA_TRIGACT_BLOCK << sam.DMAC_CHANNEL_CHCTRLA_TRIGACT_Pos) |
+			(sam.DMAC_CHANNEL_CHCTRLA_BURSTLEN_SINGLE << sam.DMAC_CHANNEL_CHCTRLA_BURSTLEN_Pos))
+	//(sam.DMAC_CHANNEL_CHCTRLA_RUNSTDBY) |
+
+	//sam.DMAC.CHANNEL[dma.Channel].CHCTRLB.Set(sam.DMAC_CHANNEL_CHCTRLB_CMD_RESUME << sam.DMAC_CHANNEL_CHCTRLB_CMD_Pos)
+
+	//sam.DMAC.CHANNEL[dma.Channel].CHINTENSET.SetBits(sam.DMAC_CHANNEL_CHINTENSET_SUSP | sam.DMAC_CHANNEL_CHINTENSET_TCMPL | sam.DMAC_CHANNEL_CHINTENSET_TERR)
+	//sam.DMAC.CHANNEL[dma.Channel].CHINTENSET.SetBits(sam.DMAC_CHANNEL_CHINTENSET_TCMPL)
+
+	sam.DMAC.CHANNEL[dma.Channel].CHCTRLA.SetBits(sam.DMAC_CHANNEL_CHCTRLA_ENABLE)
+}
+
+func (dma DMA) Trigger() {
+	sam.DMAC.SWTRIGCTRL.SetBits(1 << dma.Channel)
+}
+
+func (dma DMA) SetAction() {
+	// block beat transaction ...
+}
+
+func (dma DMA) SetDescriptor(desc DMADescriptor) {
+	DmaDescriptorSection[dma.Channel] = desc
+}
+
+func (dma DMA) Wait() {
+	for !sam.DMAC.CHANNEL[dma.Channel].CHINTFLAG.HasBits(sam.DMAC_CHANNEL_CHINTFLAG_TCMPL) {
+	}
+	sam.DMAC.CHANNEL[dma.Channel].CHINTFLAG.SetBits(sam.DMAC_CHANNEL_CHINTFLAG_TCMPL)
+}
+
+type DMADescriptorHelper struct {
+	StepSize uint8          // Address Increment Step Size : 1, 2, 4, 8, 16, 32, 64, 128
+	StepSel  uint8          // Step Selection : 0=DST, 1=SRC
+	DstInc   bool           // Destination Address Increment Enable
+	SrcInc   bool           // Source Address Increment Enable
+	BeatSize uint8          // Beat Size : 1=BYTE, 2:HWORD, 4:WORD
+	BlockAct uint8          // Block Action : 0=NOACT, 1=INT, 2=SUSPEND, 3=BOTH
+	EvoSel   uint8          // Event Output Selection : 0=DISABLE, 1=BLOCK, 3:BEAT
+	Valid    bool           // Descriptor Valid
+	Length   uint32         // Length is `BTCNT * BeatSize` or `BTCNT * BeatSize * StepSize`
+	SrcAddr  unsafe.Pointer // Block Transfer Source Address
+	DstAddr  unsafe.Pointer // Block Transfer Destination Address
+	DescAddr unsafe.Pointer // Next Descriptor Address (must be 128-bit aligned)
+}
+
+func GetDMADescriptorHelper() *DMADescriptorHelper {
+	ret := &DMADescriptorHelper{
+		StepSize: 1,
+		StepSel:  0,
+		DstInc:   false,
+		SrcInc:   false,
+		BeatSize: 1,
+		BlockAct: 0,
+		EvoSel:   0,
+		Valid:    true,
+		Length:   0,
+		SrcAddr:  unsafe.Pointer(nil),
+		DstAddr:  unsafe.Pointer(nil),
+		DescAddr: unsafe.Pointer(nil),
+	}
+
+	return ret
+}
+
+func (dma DMA) NewDescriptor(src unsafe.Pointer, dst unsafe.Pointer, beatSize uint8, srcInc, dstInc bool, stepSize uint8, stepSrc bool, size uint16) *DMADescriptor {
+	si := uint16(0)
+	if srcInc {
+		si = 1
+	}
+
+	di := uint16(0)
+	if dstInc {
+		di = 1
+	}
+
+	ssel := uint16(0)
+	if stepSrc {
+		ssel = 1
+	}
+
+	ss := uint16(0)
+	switch stepSize {
+	case 1:
+		ss = 0
+	case 2:
+		ss = 1
+	case 4:
+		ss = 2
+	case 8:
+		ss = 3
+	case 16:
+		ss = 4
+	case 32:
+		ss = 5
+	case 64:
+		ss = 6
+	case 128:
+		ss = 7
+	default:
+		// TODO: error
+		ss = 0
+	}
+
+	bs := uint16(0)
+	switch beatSize {
+	case 1:
+		bs = 0
+	case 2:
+		bs = 1
+	case 4:
+		bs = 2
+	default:
+		// TODO: error
+		bs = 0
+	}
+
+	// 2
+	//go:align 16
+	DmaDescriptorSection[1] = DMADescriptor{
+		btctrl: (1 << 0) | // VALID: Descriptor Valid
+			(0 << 1) | // EVOSEL=DISABLE: Event Output Selection
+			(0 << 3) | // BLOCKACT=NOACT: Block Action
+			(bs << 8) | // BEATSIZE: Beat Size
+			(si << 10) | // SRCINC: Source Address Increment Enable
+			(di << 11) | // DSTINC: Destination Address Increment Enable
+			(ssel << 12) | // STEPSEL: Step Selection
+			(ss << 13), // STEPSIZE: Address Increment Step Size
+		btcnt:    size >> bs,
+		Descaddr: 0,
+	}
+
+	if stepSrc {
+		// STEPSEL == SRC
+		DmaDescriptorSection[1].srcaddr = uint32(uintptr(src) + uintptr((size)<<ss))
+		DmaDescriptorSection[1].dstaddr = uint32(uintptr(dst) + uintptr(size))
+	} else {
+		// STEPSEL == DST
+		DmaDescriptorSection[1].srcaddr = uint32(uintptr(src) + uintptr(size))
+		DmaDescriptorSection[1].dstaddr = uint32(uintptr(dst) + uintptr((size)<<ss))
+	}
+
+	return &DmaDescriptorSection[1]
+}
---
+++ examples/memcpy/memcpy.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"device/sam"
+	"fmt"
+	"machine"
+	"time"
+	"unsafe"
+
+	dma "github.com/sago35/tinygo-dma"
+)
+
+const (
+	bufSize = 1024
+)
+
+var (
+	source      [bufSize]byte
+	destination [bufSize]byte
+)
+
+func main() {
+	led := machine.LED
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	d5 := machine.D5
+	d5.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	d6 := machine.D6
+	d6.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	// wait for USB-CDC
+	time.Sleep(5 * time.Second)
+
+	fmt.Printf("- %b\r\n", sam.DMAC.CHANNEL[0].CHSTATUS.Get())
+	d := dma.NewDMA(func(d dma.DMA) {
+		d6.Toggle()
+		d.Trigger()
+		return
+	})
+
+	// Only software/event triggers
+	//d.SetTrigger(0)
+
+	// やっぱりいらないものが多いので、 DMAConfig{} を作って処理するようにしたい
+	// func (c *DMAConfig) Add(a DMAConfig) も出来るようにしたい
+	// 最終的に d.AddDescriptor(c) して link も出来るようにする
+	// といいつつ、 chan 毎の最初の 1 個目の descriptor はアドレスがある程度固定なので
+	//   c := DMADescriptor{}
+	//   c.Add(DMADescriptor{})
+	//   d.SetDescriptor(c)
+	// みたいなのが良いかも
+	// 最終、決められたアドレスの場所の descriptor に書く必要があるのでコピーしつつ
+	// channel 毎に enable にしていく必要がありそう
+	// なんでも設定できる I/F を定義するのはかなり難しい感じなので、
+	// 用途を絞って SPI tx に対してはこれ、みたいなのを定義するのが楽そう
+	d.AddDescriptor(unsafe.Pointer(&source[0]), unsafe.Pointer(&destination[0]),
+		1,       // Beat Size
+		true,    // Source Address Increment
+		true,    // Destination Address Increment
+		1,       // Step Size
+		false,   // Step Selection (true: source, false: destination)
+		bufSize, // Total size of DMA transfer
+	)
+
+	desc2 := d.NewDescriptor(unsafe.Pointer(&source[0]), unsafe.Pointer(&destination[9]),
+		1,         // Beat Size
+		true,      // Source Address Increment
+		true,      // Destination Address Increment
+		1,         // Step Size
+		false,     // Step Selection (true: source, false: destination)
+		bufSize-9, // Total size of DMA transfer
+	)
+
+	for i := range source {
+		source[i] = 0xFF - byte(i)
+	}
+
+	if true {
+		fmt.Printf("new %08X\r\n", unsafe.Pointer(&desc2))
+		dma.DmaDescriptorSection[d.Channel].Descaddr = uint32(uintptr(unsafe.Pointer(desc2)))
+		fmt.Printf("%#v\r\n", dma.DmaDescriptorSection[0])
+		fmt.Printf("%#v\r\n", dma.DmaDescriptorSection[1])
+	}
+
+	fmt.Printf("%b\r\n", sam.DMAC.CHANNEL[0].CHSTATUS.Get())
+	fmt.Printf("INTSTATUS %08X\r\n", sam.DMAC.INTSTATUS.Get())
+	d.Start()
+
+	fmt.Printf("--\r\n")
+	fmt.Printf("%b\r\n", sam.DMAC.CHANNEL[0].CHSTATUS.Get())
+	fmt.Printf("pend: %b\r\n", sam.DMAC.PENDCH.Get())
+	fmt.Printf("source %v %v\r\n", source[:8], source[8:16])
+	fmt.Printf("before %v %v\r\n", destination[:8], destination[8:16])
+	fmt.Printf("INTSTATUS %08X\r\n", sam.DMAC.INTSTATUS.Get())
+	fmt.Printf("INTPEND %04X\r\n", sam.DMAC.INTPEND.Get())
+	d5.High()
+	d6.Toggle()
+	d.Trigger()
+	fmt.Printf("INTPEND %04X\r\n", sam.DMAC.INTPEND.Get())
+	fmt.Printf("%b\r\n", sam.DMAC.CHANNEL[0].CHSTATUS.Get())
+	fmt.Printf("pend: %b\r\n", sam.DMAC.PENDCH.Get())
+	time.Sleep(1 * time.Millisecond)
+	//sam.DMAC.CHANNEL[0].CHCTRLB.Set(0x02)
+	fmt.Printf("%b\r\n", sam.DMAC.CHANNEL[0].CHSTATUS.Get())
+	fmt.Printf("pend: %b\r\n", sam.DMAC.PENDCH.Get())
+	//time.Sleep(1 * time.Second)
+	//d.Wait()
+	//d.Trigger()
+	//time.Sleep(1 * time.Second)
+	fmt.Printf("%b\r\n", sam.DMAC.CHANNEL[0].CHSTATUS.Get())
+	//d.Wait()
+	d5.Low()
+	//time.Sleep(5 * time.Second)
+	fmt.Printf("after  %v %v\r\n", destination[:8], destination[8:16])
+	fmt.Printf("%b\r\n", sam.DMAC.CHANNEL[0].CHSTATUS.Get())
+
+	for i := range source {
+		destination[i] = 0
+	}
+
+	//d5.High()
+	for i := range source {
+		destination[i] = source[i]
+	}
+	//d5.Low()
+	fmt.Printf("after2 %v %v\r\n", destination[:8], destination[8:16])
+
+	for {
+		led.Toggle()
+		time.Sleep(100 * time.Millisecond)
+	}
+}
